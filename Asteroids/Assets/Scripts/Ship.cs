71606b2dbbad79d720ca067dd81deb4597c78dc7
Lyndon Panton <LyndonPanton@gmail.com>
2023-02-12 00:55:20 +0000

    Ship is only destroyed on asteroid collision

--- Asteroids/Assets/Scripts/Ship.cs
+++ Asteroids/Assets/Scripts/Ship.cs
@@ -64,6 +64,9 @@ public class Ship : MonoBehaviour
     /// <param name="collision"></param>
     void OnCollisionEnter2D(Collision2D collision)
     {
-        Destroy(gameObject);
+        if (collision.gameObject.CompareTag("Asteroid"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
